de0b5747f1fc9cf327f1ba0da105b4068e9c1168
vicelemon1841 <lsk18415@naver.com>
2026-02-05 17:22:57 +0900

    .nextLine()에 null이 들어올 때(입력이 더 이상 없을 때) 합산이 종료되는 문제 (EOF)

---
+++ 10951.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true){
+            String line = br.readLine();
+            if(line==null){
+                break;
+            }
+
+            StringTokenizer nums = new StringTokenizer(line);
+            int cnt = nums.countTokens();
+            int sum = 0;
+            for(int i =0; i<cnt;i++){
+                sum =sum + Integer.parseInt(nums.nextToken()); 
+            }
+
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
