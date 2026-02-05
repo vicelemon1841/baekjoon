56a24928a008ed2971f5d28567337bc226095dca
vicelemon1841 <lsk18415@naver.com>
2026-02-05 17:07:05 +0900

    AXB

---
+++ 10998.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a*b);
+    }
+}
