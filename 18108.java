292c9c2495c929504396d72f239802b510f7a371
vicelemon1841 <lsk18415@naver.com>
2026-02-05 17:08:26 +0900

    식을 세워서 직접 계산하는 문제

---
+++ 18108.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        Scanner scan = new Scanner(System.in);
+        int y = scan.nextInt();
+
+        int m = 2541-1998;
+
+        System.out.println(y-m);
+    }
+}
