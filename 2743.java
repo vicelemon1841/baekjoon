52ce4403b6bb95f2a374d8af27c39537235f69a9
vicelemon1841 <lsk18415@naver.com>
2026-02-10 17:28:56 +0900

    단어를 받아서 그 길이를 출력하는 문제

---
+++ 2743.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line = br.readLine();
+
+        bw.write(String.valueOf(line.length()));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
