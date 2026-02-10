6c40c9d01ebd491bf7601f77474111ae8d58242d
vicelemon1841 <lsk18415@naver.com>
2026-02-10 17:22:38 +0900

    단어와 숫자 n을 받고 그 단어의 n번째 알파벳을 출력하는 문제

---
+++ 27866.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line = br.readLine();
+        int num = Integer.parseInt(br.readLine());
+        bw.write(line.charAt(num-1));
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+    }
+}
